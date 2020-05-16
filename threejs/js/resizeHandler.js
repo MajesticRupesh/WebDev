8219258184d035330a600578707796a34e242c7f
Rupesh Pal <rupcomp1@gmail.com>
2020-05-16 20:49:10 +0530

    realistic raining added

---
+++ threejs/js/resizeHandler.js
@@ -0,0 +1,8 @@
+/* This function handles the viewport when it is resized */
+window.addEventListener('resize', function() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    renderer.setSize(width, height);
+    camera.aspect = width/height;
+    camera.updateProjectionMatrix();
+});
